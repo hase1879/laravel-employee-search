06338c71f77ce6fd5340d0613a85ba8a5bca4af9
hase1879 <kamhuta1438@gmail.com>
2023-04-13 11:17:04 +0900

    laravel-admin 修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
             $table->string('zip_code')->nullable();;
             $table->string('present_address')->nullable();;
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
